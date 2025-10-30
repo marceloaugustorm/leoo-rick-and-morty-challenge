0f6c02af52f54e4291fc8dddbacec2a4183b090d
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-10-30 16:04:34 -0300

    continue no tratamento de erro

--- api.py
+++ api.py
@@ -2,32 +2,35 @@ import requests
 import csv
 
 personagens = []
-for i in range(1,51):
+
+for i in range(1, 51):
     url = f"https://rickandmortyapi.com/api/character/{i}"
-
+    
     response = requests.get(url)
-
+    
     if response.status_code != 200:
-        print(f"Erro ao usar a API", response.status_code)
-
+        print(f"Erro ao acessar personagem {i}: Status {response.status_code}")
+        continue  
+    
     dados = response.json()
-
     personagens.append(dados)
 
+arquivo = "personagens.csv"
 
-    arquivo = "personagens.csv"
-
-with open(arquivo, mode="w", newline="", encoding = "utf-8") as file:
+with open(arquivo, mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file, delimiter=";")
+    
+  
     writer.writerow(["id", "name", "status", "species", "type", "gender"])
     
     for personagem in personagens:
         writer.writerow([
             personagem["id"],
-            personagem["name"],
-            personagem["status"],
-            personagem["species"],
-            personagem["type"],
-            personagem["gender"],
-            ])
+            f"{personagem['name']}",  
+            f"{personagem['status']}",
+            f"{personagem['species']}",
+            f"{personagem['type']}",
+            f"{personagem['gender']}"
+        ])
 
+print(f"Arquivo '{arquivo}' criado com sucesso! Total: {len(personagens)} personagens.")
