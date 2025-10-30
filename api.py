27b140f18eab0e41601c2205e8547dd454fd6c82
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-10-30 15:02:36 -0300

    adicionando o csv

--- api.py
+++ api.py
@@ -1,25 +1,33 @@
 import requests
-
-i = 0
+import csv
 
 personagens = []
-
 for i in range(1,51):
     url = f"https://rickandmortyapi.com/api/character/{i}"
+
     response = requests.get(url)
 
     if response.status_code != 200:
-        print("Erro ao acessar a API", response.status_code)
-        continue 
-    
+        print(f"Erro ao usar a API", response.status_code)
+
     dados = response.json()
+
     personagens.append(dados)
 
-    print(dados["name"])
 
+    arquivo = "personagens.csv"
 
-
-
+with open(arquivo, mode="w", newline="", encoding = "utf-8") as file:
+    writer = csv.writer(file, delimiter=";")
+    writer.writerow(["id", "name", "status", "species", "type", "gender"])
     
-
+    for personagem in personagens:
+        writer.writerow([
+            personagem["id"],
+            personagem["name"],
+            personagem["status"],
+            personagem["species"],
+            personagem["type"],
+            personagem["gender"],
+            ])
 
